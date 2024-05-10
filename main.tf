55a1627554fc4a469891ca028cf30587f3361a15
Vadim Nosovsky <32557641+vnosovsky@users.noreply.github.com>
2024-05-10 14:33:03 -0700

    Update main.tf typo

--- main.tf
+++ main.tf
@@ -42,7 +42,7 @@ resource "aws_security_group_rule" "blog_http_in" {
   protocol = "tcp"
   cidr_blocks = ["0.0.0.0/0"]
 
-  aws_security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 } 
 
 resource "aws_security_group_rule" "blog_https_in" {
