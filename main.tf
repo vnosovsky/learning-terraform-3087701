6504c79410178bd6c4cfc0d1a12d4f70f3866152
Vadim Nosovsky <32557641+vnosovsky@users.noreply.github.com>
2024-05-10 14:24:37 -0700

    typo

--- main.tf
+++ main.tf
@@ -29,38 +29,38 @@ resource "aws_instance" "blog" {
   }
 }
 
-resource "aws_secirity_group" "blog" {
+resource "aws_security_group" "blog" {
   name = "blog"
   description = "allow http and https in, allow everything out"
   vpc_id = data.aws_vpc.default.id
 }
 
-resource "aws_secirity_group_rule" "blog_http_in" {
+resource "aws_security_group_rule" "blog_http_in" {
   type = "ingress"
   from_port = 80
   to_port = 80
   protocol = "tcp"
   cidr_block = ["0.0.0.0/0"]
 
-  aws_secirity_group_id = aws_secirity_group.blog.id
+  aws_secirity_group_id = aws_security_group.blog.id
 } 
 
-resource "aws_secirity_group_rule" "blog_https_in" {
+resource "aws_security_group_rule" "blog_https_in" {
   type = "ingress"
   from_port = 443
   to_port = 443
   protocol = "tcp"
   cidr_block = ["0.0.0.0/0"]
 
-  aws_secirity_group_id = aws_secirity_group.blog.id
+  aws_secirity_group_id = aws_security_group.blog.id
 } 
 
-resource "aws_secirity_group_rule" "blog_everything_out" {
+resource "aws_security_group_rule" "blog_everything_out" {
   type = "egress"
   from_port = 0
   to_port = 0
   protocol = "-1"
   cidr_block = ["0.0.0.0/0"]
 
-  aws_secirity_group_id = aws_secirity_group.blog.id
+  aws_secirity_group_id = aws_security_group.blog.id
 } 
