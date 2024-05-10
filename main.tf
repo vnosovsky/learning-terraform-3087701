442a5384a5407ec1f91d5cb605af46e9994df873
Vadim Nosovsky <32557641+vnosovsky@users.noreply.github.com>
2024-05-10 15:26:33 -0700

    create new dev vpc
    remove security_group_id
    
    remove target_groups
    
    autoscaling
    
    load balancer

--- main.tf
+++ main.tf
@@ -18,30 +18,81 @@ data "aws_vpc" "default" {
   default = true
 }
 
-resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
 
-  vpc_security_group_ids = [module.blog_sg.security_group_id]
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Name = "HelloWorld.new"
+    Terraform = "true"
+    Environment = "dev"
   }
 }
 
-resource "aws_security_group" "blog" {
+
+module "autoscaling" {
+  source  = "terraform-aws-modules/autoscaling/aws"
+  version = "7.4.1"
+  # insert the 1 required variable here
+
   name = "blog"
-  description = "allow http and https in, allow everything out"
-  vpc_id = data.aws_vpc.default.id
+  min_size = 1
+  max_size = 1
+
+  vpc_zone_identifier = module.blog_vpc.public_subnets
+  # target_groups = module.blog_alb.target_group_arns
+  security_groups = [module.blog_sg.security_group_id]
+
+  image_id           = data.aws_ami.app_ami.id
+  instance_type = var.instance_type
 }
 
+module "blog-alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name    = "blog-alb"
+  vpc_id  = module.blog_vpc.vpc_id
+  subnets = module.blog_vpc.public_subnets
+  security_groups = [module.blog_sg.security_group_id]
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      redirect = {
+        port        = "443"
+        protocol    = "HTTPS"
+        status_code = "HTTP_301"
+      }
+    }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix      = "blog-"
+      protocol         = "HTTP"
+      port             = 80
+      target_type      = "instance"
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    Project     = "Example"
+  }
+}
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
+  vpc_id = module.blog_vpc.vpc_id
   name = "blog"
 
-  security_group_id = aws_security_group.blog.id
+  # security_group_id = aws_security_group.blog.id
   
   ingress_rules = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
