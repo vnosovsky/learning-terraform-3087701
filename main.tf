2169a0c4e9d70fca49a76c4d7caa7a6356b042e9
Vadim Nosovsky <32557641+vnosovsky@users.noreply.github.com>
2024-05-10 18:16:15 -0700

    adding load balancer

--- main.tf
+++ main.tf
@@ -37,18 +37,18 @@ module "blog_vpc" {
   name = "dev"
   cidr = "10.0.0.0/16"
 
-  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+  azs            = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
 
   tags = {
-    Terraform = "true"
+    Terraform   = "true"
     Environment = "dev"
   }
 }
 
 resource "aws_instance" "blog" {
-  ami           = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  ami                    = data.aws_ami.app_ami.id
+  instance_type          = var.instance_type
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   subnet_id = module.blog_vpc.public_subnets[0]
@@ -63,6 +63,91 @@ resource "aws_instance" "blog" {
 #   description = "allow http and https in, allow everything out"
 #   vpc_id      = data.aws_vpc.default.id
 # }
+
+
+
+module "alb" {
+  source = "terraform-aws-modules/alb/aws"
+
+  name            = "blog-alb"
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
+
+  # Security Group
+  security_group_ingress_rules = {
+    all_http = {
+      from_port   = 80
+      to_port     = 80
+      ip_protocol = "tcp"
+      description = "HTTP web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+    all_https = {
+      from_port   = 443
+      to_port     = 443
+      ip_protocol = "tcp"
+      description = "HTTPS web traffic"
+      cidr_ipv4   = "0.0.0.0/0"
+    }
+  }
+  security_group_egress_rules = {
+    all = {
+      ip_protocol = "-1"
+      cidr_ipv4   = "10.0.0.0/16"
+    }
+  }
+
+  # access_logs = {
+  #   bucket = "my-alb-logs"
+  # }
+
+  listeners = {
+    ex-http-https-redirect = {
+      port     = 80
+      protocol = "HTTP"
+      # redirect = {
+      #   port        = "443"
+      #   protocol    = "HTTPS"
+      #   status_code = "HTTP_301"
+      # }
+    }
+    # ex-http-https-redirect = {
+    #   port     = 80
+    #   protocol = "HTTP"
+    #   redirect = {
+    #     port        = "443"
+    #     protocol    = "HTTPS"
+    #     status_code = "HTTP_301"
+    #   }
+    # }
+    # ex-https = {
+    #   port            = 443
+    #   protocol        = "HTTPS"
+    #   certificate_arn = "arn:aws:iam::123456789012:server-certificate/test_cert-123456789012"
+
+    #   forward = {
+    #     target_group_key = "ex-instance"
+    #   }
+    # }
+  }
+
+  target_groups = {
+    ex-instance = {
+      name_prefix = "blog-"
+      protocol    = "HTTP"
+      port        = 80
+      target_type = "instance"
+    }
+  }
+
+  tags = {
+    Environment = "dev"
+    # Project     = "Example"
+  }
+}
+
+
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
