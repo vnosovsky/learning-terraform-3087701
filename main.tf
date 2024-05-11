ddc50a945bd61f8654fe329ec77783a71a6e9b0c
Vadim Nosovsky <32557641+vnosovsky@users.noreply.github.com>
2024-05-10 17:59:55 -0700

    create new vpc

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ terraform {
   }
 }
 
-
 data "aws_ami" "app_ami" {
   most_recent = true
 
@@ -27,34 +26,52 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
-data "aws_vpc" "default" {
-  default = true
+# data "aws_vpc" "default" {
+#   default = true
+# }
+
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
 }
 
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-
   vpc_security_group_ids = [module.blog_sg.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld.new"
   }
 }
 
-resource "aws_security_group" "blog" {
-  name        = "blog"
-  description = "allow http and https in, allow everything out"
-  vpc_id      = data.aws_vpc.default.id
-}
-
+# resource "aws_security_group" "blog" {
+#   name        = "blog"
+#   description = "allow http and https in, allow everything out"
+#   vpc_id      = data.aws_vpc.default.id
+# }
 
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.2"
   name    = "blog"
 
-  security_group_id = aws_security_group.blog.id
+  vpc_id = module.blog_vpc.vpc_id
+
+  # security_group_id = aws_security_group.blog.id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
